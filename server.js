15bca9a64af8d2708f627cd433bd7f18f4d6caa8
Yobany <yobanydesign@gmail.com>
2019-02-14 09:41:43 -0500

    Added articles route for getting all articles from the db. Set up models for exporting.

--- server.js
+++ server.js
@@ -53,8 +53,24 @@ app.get("/scrape", function(req, res) {
     // Send a message to the client
     res.send("Scrape Complete");
     });
-
 });
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+    .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+
 
 // Start the server
 app.listen(PORT, function() {
