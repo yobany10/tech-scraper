c16a5d1f37bdd3101bc73db9794d803dc1e52ea3
Yobany <yobanydesign@gmail.com>
2019-02-18 21:16:17 -0500

    Added and configured morgan middleware.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var expressHB = require("express-handlebars");
 var mongoose = require("mongoose");
+var logger = require("morgan");
 var cheerio = require("cheerio");
 var axios = require("axios");
 
@@ -11,6 +12,16 @@ var PORT = 3000;
 
 // Initialize express
 var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/techCrunchDB", {useNewUrlParser: true});
