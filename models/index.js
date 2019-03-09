5560f1e333b41ecb3308892f6084e8bffc579f2d
Yobany <yobanydesign@gmail.com>
2019-03-09 12:45:33 -0500

    restored model index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("Article.js"),
-    Note: require("Note.js")
+    Article: require("./Article"),
+    Note: require("./Note")
 };
